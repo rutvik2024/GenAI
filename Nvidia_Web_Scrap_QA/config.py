a8bb50be6d9ddce44ff96680113369843c31d54c
Rutvik Jethava <133942995+rutvik2024@users.noreply.github.com>
2024-07-16 17:04:06 +0530

    Delete Nvidia_Web_Scrap_QA directory

--- Nvidia_Web_Scrap_QA/config.py
+++
@@ -1,7 +0,0 @@
-json_file_path = "Your generated json_file_path"
-csv_file_path = "./new_embeddings.csv"
-
-sentence_model_name = 'all-MiniLM-L6-v2'
-rerank_model_name = "cross-encoder/ms-marco-MiniLM-L-6-v2"
-
-llama_model_name = "meta-llama/Meta-Llama-3-8B"
